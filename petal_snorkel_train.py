9c439851e49a72e0b270d757ab3c1e48e7e09f5d
Paht J <paht.juangphanich@gmail.com>
2021-11-25 01:59:35 -0500

    minor formatting

--- petal_snorkel_train.py
+++ petal_snorkel_train.py
@@ -8,23 +8,25 @@
 # <ARalevski, 10-17-2021, updated cardinality>
 # Authors: alexandra.ralevski@gmail.com, paht.juangphanich@nasa.gov
 # ------------------------------------------------- #
-import sys, wget, pickle, json
-sys.path.insert(0,'../snorkel')
-from snorkel.labeling.model import LabelModel
-from snorkel.labeling import PandasLFApplier
-from snorkel.labeling.model import MajorityLabelVoter
-from ast import literal_eval
-from utils import smaller_models
-from create_labeling_functions import create_labeling_functions
-from tqdm import trange
-import pandas as pd 
-import os.path as osp
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.linear_model import LogisticRegression
+import numpy as np
+import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import CountVectorizer
+import os.path as osp
+from tqdm import trange
 from create_labeling_functions import create_labeling_functions
-import numpy as np 
-#import csv file and load train/test/split of dataset
+from utils import smaller_models
+from ast import literal_eval
+from snorkel.labeling.model import MajorityLabelVoter
+from snorkel.labeling import PandasLFApplier
+from snorkel.labeling.model import LabelModel
+import sys
+import wget
+import pickle
+import json
+sys.path.insert(0, '../snorkel')
+# import csv file and load train/test/split of dataset
 
 golden_json_url = 'https://raw.githubusercontent.com/nasa-petal/data-collection-and-prep/main/golden.json'
 
@@ -34,7 +36,7 @@ small_models = 'small_models_trained.pickle'
 if not osp.exists(filename):
     wget.download(golden_json_url)
 
-with open(filename,'r') as f:
+with open(filename, 'r') as f:
     golden_json = json.load(f)
 
 '''
@@ -43,33 +45,37 @@ with open(filename,'r') as f:
 datalist = list()
 for paper in golden_json:
     data = dict()
-    data['text'] = ' '.join(literal_eval(paper['title']) + literal_eval(paper['abstract']))
+    data['text'] = ' '.join(literal_eval(
+        paper['title']) + literal_eval(paper['abstract']))
     data['doi'] = paper['doi']
     data['paperid'] = paper['paper']
     datalist.append(data)
 df = pd.DataFrame(datalist)
 
 df_bio = pd.read_csv(r'./biomimicry_functions_enumerated.csv')
-labels = dict(zip(df_bio['function_enumerated'].tolist(),df_bio['function'].tolist()))
+labels = dict(
+    zip(df_bio['function_enumerated'].tolist(), df_bio['function'].tolist()))
 
-labeling_function_list = create_labeling_functions(r'./biomimicry_functions_enumerated.csv', r'./biomimicry_function_rules.csv')
+labeling_function_list = create_labeling_functions(
+    r'./biomimicry_functions_enumerated.csv', r'./biomimicry_function_rules.csv')
 applier = PandasLFApplier(lfs=labeling_function_list)
 L_golden = applier.apply(df=df)
 
-labels_overlap, L_matches, translators, translators_to_str, L_match_all, global_translator,global_translator_str, dfs = smaller_models(L_golden,5,2,labels_list=labels,df=df)
-# loop through all Golden JSON and Predict 
+labels_overlap, L_matches, translators, translators_to_str, L_match_all, global_translator, global_translator_str, dfs = smaller_models(
+    L_golden, 5, 2, labels_list=labels, df=df)
+# loop through all Golden JSON and Predict
 if not osp.exists('golden_lf.pickle'):
-    with open('golden_lf.pickle','wb') as f:
-        pickle.dump({'L_golden':L_golden,'labels_overlap':labels_overlap,'L_matches':L_matches, 
-                    'translators':translators,'translators_to_str':translators_to_str, 
-                    'L_matches_all':L_match_all, 'global_translator':global_translator, 
-                    'global_translator_str':global_translator_str,'dfs':dfs},f)
+    with open('golden_lf.pickle', 'wb') as f:
+        pickle.dump({'L_golden': L_golden, 'labels_overlap': labels_overlap, 'L_matches': L_matches,
+                    'translators': translators, 'translators_to_str': translators_to_str,
+                     'L_matches_all': L_match_all, 'global_translator': global_translator,
+                     'global_translator_str': global_translator_str, 'dfs': dfs}, f)
 
-with open('golden_lf.pickle','rb') as f:
-    data = pickle.load(f) 
+with open('golden_lf.pickle', 'rb') as f:
+    data = pickle.load(f)
     L_golden = data['L_golden']
     print("Unique Matches in golden.json: ")
-    print(*np.unique(L_golden).tolist(), sep = ", ")
+    print(*np.unique(L_golden).tolist(), sep=", ")
     L_matches = data['L_matches']
     labels_overlap = data['labels_overlap']
     translators = data['translators']
@@ -83,36 +89,41 @@ with open('golden_lf.pickle','rb') as f:
 '''
 if not osp.exists(small_models):
     models = list()
-    for i in trange(len(L_matches),desc="training small models"):
+    for i in trange(len(L_matches), desc="training small models"):
         L_match = L_matches[i]
         # TODO split the dataset so theres an equal amount of all labels
         L_train = L_match
-        L_test = L_match 
+        L_test = L_match
 
         labels = labels_overlap[i]
-        cardinality = len(labels)   # How many labels to predict 
+        cardinality = len(labels)   # How many labels to predict
         majority_model = MajorityLabelVoter(cardinality=cardinality)
-        preds_train = majority_model.predict(L=L_train)                 # Looks at each text and sees which label is predicted the most 
-        
-        # Train LabelModel - this outputs probabilistic floats 
-        label_model = LabelModel(cardinality=cardinality, verbose=True, device = 'cpu')
+        # Looks at each text and sees which label is predicted the most
+        preds_train = majority_model.predict(L=L_train)
+
+        # Train LabelModel - this outputs probabilistic floats
+        label_model = LabelModel(
+            cardinality=cardinality, verbose=True, device='cpu')
         label_model.fit(L_train=L_train, n_epochs=350, log_freq=50, seed=123)
-        probs_train = label_model.predict_proba(L=L_train)  # This gives you the probability of which label paper falls under 
+        # This gives you the probability of which label paper falls under
+        probs_train = label_model.predict_proba(L=L_train)
 
-        models.append(label_model) # this label model can help predict the type of paper
+        # this label model can help predict the type of paper
+        models.append(label_model)
 
-    with open(small_models,'wb') as f:
-        pickle.dump({"Label_models":models, 'labels_overlap':labels_overlap,
-            'translators':translators,'translators_to_str':translators_to_str,
-            'texts_df':dfs},f)
+    with open(small_models, 'wb') as f:
+        f
+        pickle.dump({"Label_models": models, 'labels_overlap': labels_overlap,
+                     'translators': translators, 'translators_to_str': translators_to_str,
+                     'texts_df': dfs}, f)
 
 # Training a single large model
 cardinality = len(global_translator)
 majority_model = MajorityLabelVoter(cardinality=cardinality)
 preds_train = majority_model.predict(L=L_match_all)
-label_model = LabelModel(cardinality=cardinality, verbose=True, device = 'cpu')
+label_model = LabelModel(cardinality=cardinality, verbose=True, device='cpu')
 label_model.fit(L_train=L_match_all, n_epochs=300, log_freq=50, seed=123)
 
-with open(large_model,'wb') as f:
-    pickle.dump({"Label_model":label_model,'global_translator':global_translator,
-                'global_translator_str':global_translator_str,'text_df':df},f)
+with open(large_model, 'wb') as f:
+    pickle.dump({"Label_model": label_model, 'global_translator': global_translator,
+                'global_translator_str': global_translator_str, 'text_df': df}, f)
